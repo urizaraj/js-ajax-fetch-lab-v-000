e7603b302c484df7573016a7184ef60cd408d3d8
Alexander Urizar <urizaraj@gmail.com>
2018-03-31 18:21:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,81 @@
+const issuesURL = 'https://api.github.com/repos/urizaraj/javascript-fetch-lab/issues'
+
 function getIssues() {
+  const url = issuesURL
+  const init = {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }
+
+  fetch(url, init)
+    .then(res => res.json())
+    .then(res => showIssues(res))
 }
 
 function showIssues(json) {
+  const issues = $('#issues')
+  json.forEach(issue => {
+    const html = `<p>${issue.title}<br>${issue.body}</p>`
+    issues.append(html)
+  });
 }
 
 function createIssue() {
+  const title = document.getElementById('title')
+  const body = document.getElementById('body')
+
+  const postData = {
+    title: title.value,
+    body: body.value
+  }
+
+  const init = {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }
+
+  const url = `https://api.github.com/repos/urizaraj/javascript-fetch-lab/issues`
+
+  fetch(url, init)
+    .then(res => res.json())
+    .then(res => getIssues())
 }
 
-function showResults(json) {
-}
+function showResults(json) {}
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const base = 'https://api.github.com/repos'
+  const url = `${base}/${repo}/forks`
+
+  const init = {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }
+
+  fetch(url, init)
+    .then(res => res.json())
+    .then(res => showForkedRepo(res))
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
+}
+
+function showForkedRepo(res) {
+  results = $('#results')
+  link = $('#link')
+  repName = $('#repName')
+
+  repName.text(res.full_name)
+  link.attr('href', res.html_url)
+  link.text('Link')
 }
